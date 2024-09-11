a806e9a130cd567a297291648365dc7f493a9903
Maciel Santos <132824821+macielphp@users.noreply.github.com>
2024-09-11 20:09:42 -0300

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+export default [
+    {
+      files: ["**/*.js"], // Specifies the files ESLint should check
+      rules: {
+        // Example rule: disallow console.log statements
+        "no-console": "warn",
+      },
+    },
+  ];
+  
